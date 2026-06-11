91ac87034dc97be54a8ea11acf9e434c277a05f4
AndreOliveira34 <andre.oliveira34@hotmail.com>
2026-06-11 15:16:21 -0300

    Create inserts.sql

---
+++ SQL/SQL/inserts.sql
@@ -0,0 +1,11 @@
+INSERT INTO clientes (nome, cidade)
+VALUES
+('João Silva', 'São Paulo'),
+('Maria Souza', 'Rio de Janeiro'),
+('Carlos Lima', 'Aracaju');
+
+INSERT INTO produtos (nome_produto, preco)
+VALUES
+('Notebook', 3500.00),
+('Mouse', 80.00),
+('Teclado', 150.00);
